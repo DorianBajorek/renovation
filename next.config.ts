27df89d819b27a43e75b44957748f74d3d1680a8
DorianBajorek <dorianb324@gmail.com>
2025-08-30 07:15:37 +0200

    remove redirect

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.remotrack.pl',
-          },
-        ],
-        destination: 'https://remotrack.pl/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
